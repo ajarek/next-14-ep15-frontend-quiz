ebf7f1f10441b775d1c95847c38e83bac66e6437
Ajarek <ajarek@poczta.onet.pl>
2023-11-16 01:02:28 +0100

    added useFetch

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,14 @@
-"use client"
-import { useState, useEffect } from 'react'
+'use client'
+import useFetch from "@/lib/getQuiz";
+import type {Data} from "@/types/data";
 
 export default function Home() {
+  const { data } =  useFetch("data.json");
 
-  interface Crate {
-    id: string;
-    nameCrate: string;
-    albums: string;
-  }
-
-  const [crates, setCrates] = useState<Array<Crate>>([]);
-
-  useEffect(() => {
-    fetch('data.json',{
-      headers : { 
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-       }
-    })
-    .then((res) => res.json())
-    .then((resJson) => { const data = resJson.quizzes
-      setCrates(data)})
-  }, [])
-
-  console.log(crates)
-
-    
- 
-  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-<h1>lol</h1>
+      {data &&
+        [...data.quizzes].map((el, index) => <p key={index}>{el.title}</p>)}
     </main>
-  )
+  );
 }
